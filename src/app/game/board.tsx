4ef98fae2f3dca08226d64674f7de05350e31d23
Nicolette Sookar <nicolettesookar@gmail.com>
2024-08-28 23:38:54 -0400

    execute logic and start of drag and drop

--- src/app/game/board.tsx
+++ src/app/game/board.tsx
@@ -2,22 +2,51 @@
 
 import { useState, useEffect } from 'react'
 
-export default function Board() {
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
+export default function Board({ instructions }) {
+
+    const [avatarPosition, setAvatarPosition] = useState({ x: 1, y: 1 })
     const [divClassName, setdivClassName] = useState(`border-white border-2 h-11 w-11`)
-
+    //const [divClassName, setdivClassName] = useState(`border-white border-2 h-11 w-11`)
 
     let rows = []
+    console.log("instructions in board: ", instructions)
+    useEffect(() => {
+        console.log("in useEffect")
+        let newX = avatarPosition.x
+        let newY = avatarPosition.y
+
+        instructions.forEach((instruction, index) => {
+            console.log('executing instruction: ', instruction)
+            newX += instruction.x
+            newY += instruction.y
+        })
+        setAvatarPosition({ x: newX, y: newY })
+        console.log(avatarPosition)
+    }, [instructions])
+
+    useEffect(() => {
+        console.log('avatar position use effect: ', avatarPosition)
+    }, [avatarPosition])
+
     for (let row = 1; row <= 10; row++) {
         for (let i = 1; i <= 10; i++) {
             let key = `${i} ${row}`
-            rows.push(<div className={divClassName} key={key}>{row},{i}</div>)
+            let isAvatar = ""
+            if (row == avatarPosition.y && i == avatarPosition.x) {
+                isAvatar = "X"
+            }
+            rows.push(<div className={divClassName} key={key}>{isAvatar}</div>)
         }
     }
 
+
     return (
-        <div className="grid grid-cols-10 h-max w-max">
-            {rows}
-        </div>
+        <>
+            <div className="grid grid-cols-10 h-max w-max">
+                {rows}
+            </div>
+        </>
     )
 }
--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,5 +1,50 @@
-import Board from "./board"
+'use client'
+import Board from "./Board"
+import Up from "./blocks/Up"
+import Down from "./blocks/Down"
+import Execute from "./Execute"
+import { DndContext } from '@dnd-kit/core';
+import DropZone  from "./Droppable"
+
+
+import { useState, useEffect } from 'react'
+
 
 export default function Page() {
-    return (<Board></Board>)
+
+    const [instructions, setInstructions] = useState([])
+    const [tempInstructions, setTempInstructions] = useState([])
+    const [parent, setParent] = useState(null);
+
+    console.log('instructions in page: ', instructions)
+
+    const handleDragEnd = (event) => {
+
+        const { active, over } = event;
+        if (over) {
+          console.log(`Dropped ${active.id} into ${over.id}`);
+          // Here you can handle the logic when the item is dropped
+          setParent(over ? over.id : null);
+
+        }
+      };
+    return (
+        <DndContext onDragEnd={handleDragEnd}>
+        <div className="flex flex-row">
+            <div className="flex-auto">
+                <Board instructions={instructions} />
+            </div>
+            <div className="flex-auto">
+                <DropZone id="dropzone1" />
+            </div>
+            <div className="flex-auto">
+                <Down tempInstructions={tempInstructions} id='down'/>
+                <br></br>
+                <Up tempInstructions={tempInstructions} />
+                <br></br>
+                <Execute instructions={instructions} tempInstructions={tempInstructions} setInstructions={setInstructions} />
+            </div>
+        </div>
+        </DndContext>
+    )
 }
